1528528f239066ca92824920f6b546a563629826
Centre for Computational Protein Engineering <CCPE@users.noreply.github.com>
2016-12-12 18:24:51 +0530

    Initial commit
    --/The Heart of Data System/-- of "Slappé®". | License — MIT, https://opensource.org/licenses/MIT.

---
+++ index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+/*!
+ * -------
+ * Slappé®
+ * -------
+ * @NPM — https://www.npmjs.com/package/slappe
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 11-Dec-2016
+ * @license   : MIT
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : version{.js} and package.json
+ * @build     : SEED™ — Skövde
+ *              └────── A Sequømics Product — http://seed.sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Data System/-- of "Slappé®".
+ * ___________________________________________________________________________
+ */
