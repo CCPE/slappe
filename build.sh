c267dc3f29f7d73e570f5d53675c636f809a7c55
Centre for Computational Protein Engineering <CCPE@users.noreply.github.com>
2016-12-12 18:01:29 +0530

    Initial commit
    build.sh, shell script to --/build/-- whole "Slappé®" in one go. » Usage: $ sh build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# ==========================++== Slappé® ==++=========================
+#
+#                                 CCPE
+#             Centre for Computational Protein Engineering
+#
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#     Copyright © 2008 - 2016, Prabhat Kumar. All rights reserved.
+# Copyright © 2014 - 2016, Sequømics Corporation, All rights reserved.
+# ====================================================================
