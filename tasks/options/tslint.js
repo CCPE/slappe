3636b39b916cec44694ad18815ef6e294ff68ba3
Centre for Computational Protein Engineering <CCPE@users.noreply.github.com>
2016-12-20 18:48:10 +0530

    A task commit.
    Usage: $ grunt lint | Using: grunt-tslint

--- tasks/options/tslint.js
+++ tasks/options/tslint.js
@@ -28,3 +28,21 @@
  * --/The Heart of Build System/-- of "Slappé®".
  * ___________________________________________________________________________
  */
+
+module.exports = {
+  options: {
+    // can be a configuration object or a filepath to `tslint.json`.
+    configuration: "./tslint.json",
+    // If set to true, tslint errors will be reported, but not fail the task.
+    // If set to false, tslint errors will be reported, and the task will fail.
+    force : false,
+    fix   : true
+  },
+  files: {
+    src: [
+      // default directory.
+      './core/type/*.ts',
+      './core/type/**/*.ts'
+    ]
+  }
+};
